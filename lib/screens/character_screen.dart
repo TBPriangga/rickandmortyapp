d73e3615a56c791d219a88101678e0430891e694
Tri Bayu Priangga <tpriangga6@gmail.com>
2024-01-17 08:26:54 +0700

    Add episode list and API provider for episodes

--- lib/screens/character_screen.dart
+++ lib/screens/character_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rickandmortyapp/view_models/api_provider.dart';
 import '../models/character_model.dart';
 
 class CharacterScreen extends StatelessWidget {
@@ -7,6 +9,7 @@ class CharacterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(character.name!),
@@ -18,16 +21,93 @@ class CharacterScreen extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 200,
+              height: size.height * 0.35,
               width: double.infinity,
               child: Hero(
                 tag: character.id!,
-                child: Image.network(character.image!),
+                child: Image.network(
+                  character.image!,
+                  fit: BoxFit.cover,
+                ),
               ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              height: size.height * 0.14,
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  cardData("Status", character.status!),
+                  cardData("Species", character.species!),
+                  cardData("Origin", character.origin!.name!),
+                ],
+              ),
+            ),
+            const Text(
+              "Episodes",
+              style: TextStyle(fontSize: 17),
+            ),
+            EpisodeList(size: size, character: character)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget cardData(String text1, String text2) {
+    return Expanded(
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(text1),
+            Text(
+              text2,
+              style: const TextStyle(overflow: TextOverflow.ellipsis),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class EpisodeList extends StatefulWidget {
+  final Size size;
+  final Character character;
+  const EpisodeList({super.key, required this.size, required this.character});
+
+  @override
+  State<EpisodeList> createState() => _EpisodeListState();
+}
+
+class _EpisodeListState extends State<EpisodeList> {
+  @override
+  void initState() {
+    super.initState();
+    final apiProvider = Provider.of<ApiProvider>(context, listen: false);
+    apiProvider.getEpisodes(widget.character);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final apiProvider = Provider.of<ApiProvider>(context);
+    return SizedBox(
+      height: widget.size.height * 0.35,
+      child: ListView.builder(
+          itemCount: apiProvider.episodes.length,
+          itemBuilder: (context, index) {
+            final episode = apiProvider.episodes[index];
+            return ListTile(
+              leading: Text(episode.episode!),
+              title: Text(episode.name!),
+              trailing: Text(episode.airDate!),
+            );
+          }),
     );
   }
 }
--- lib/view_models/api_provider.dart
+++ lib/view_models/api_provider.dart
@@ -2,12 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:rickandmortyapp/models/episode_model.dart';
 import '../models/character_model.dart';
 
 class ApiProvider with ChangeNotifier {
   final url = 'rickandmortyapi.com';
   List<Character> characters = [];
+  List<EpisodeModel> episodes = [];
 
   Future<void> getCharacters(int page) async {
     final resutl = await http
@@ -15,5 +16,16 @@ class ApiProvider with ChangeNotifier {
     final response = characterModelFromJson(resutl.body);
     characters.addAll(response.results!);
     notifyListeners();
+  }
+
+  Future<List<EpisodeModel>> getEpisodes(Character character) async {
+    episodes = [];
+    for (var i = 0; i < character.episode!.length; i++) {
+      final result = await http.get(Uri.parse(character.episode![i]));
+      final response = episodeModelFromJson(result.body);
+      episodes.add(response);
+      notifyListeners();
+    }
+    return episodes;
   }
 }
